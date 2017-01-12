2eb05ba8c475ab72ded62f95a67124f3aede751a
Tom Trahan <tom@shippable.com>
2017-01-12 15:09:34 -0800

    sets aws credentials to new profile name and sets global variable

--- pipeline/install/installAwsCli.sh
+++ pipeline/install/installAwsCli.sh
@@ -3,8 +3,11 @@
 echo "installing AWS CLI..."
 
 # add AWS credentials
-- mkdir ~/.aws
-- echo -e "[default]\naws_access_key_id=$AWS_ACCESS_KEY_ID\naws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials2
+if [[ ! -d ~/.aws ]]; then
+  mkdir ~/.aws
+fi
+echo -e "[gitlabCi]\naws_access_key_id=$AWS_ACCESS_KEY_ID\naws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+export AWS_DEFAULT_PROFILE=gitlabCi
 
 # install AWS CLI
 # install Python and PIP if not installed
