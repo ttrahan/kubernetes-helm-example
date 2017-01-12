e047933f09bb55c86b473ba6bde005b3647b9a79
Tom Trahan <tom@shippable.com>
2017-01-11 17:05:09 -0800

    update for kube deploy from gitlab ci

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #! /bin/bash -e
 
 # source environment variables (these come from shippable.resources.yml when running via Shippable pipeline)
-set -a
-source ./variables.env
-set +a
+# set -a
+# source ./variables.env
+# set +a
 
 # for each yaml template, replace environment variables with values
-for file in deployTemplates/*.yaml; do
+for file in deploy/deployTemplates/*.yaml; do
   TEMPLATE=$file
   DEST=$(echo $(basename $file) | sed 's/-template//')
-  envsubst < $TEMPLATE > deploySpecs/$DEST
+  envsubst < $TEMPLATE > deploy/deploySpecs/$DEST
 done;
 
 # for each deploySpec, execute deployment to Kube cluster
-for file in deploySpecs/*.yaml; do
+for file in deploy/deploySpecs/*.yaml; do
   echo "processing "$file
   # baseFile=${file#*/}
   # baseFileNoExt=${baseFile%.yaml}
--- install.sh
+++ install.sh
@@ -17,8 +17,16 @@ if [[ ! $(which python) ]]; then
   sudo python27 get-pip.py
 fi
 # install AWS CLI
-sudo $(which pip) install awscli
+if [[ ! $(which aws) ]]; then
+  sudo $(which pip) install awscli
+  if [[ $(aws help) ]]; then
+    echo "AWS CLI installed successfully"
+  fi
+fi
 
-if [[ $(aws help) ]]; then
-  echo "AWS CLI installed successfully"
+# install Kubernetes CLI
+if [[ ! $(which kubectl) ]]; then
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
 fi
