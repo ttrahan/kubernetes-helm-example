22249aafe9bbcd0726ef87a76527943763417a96
Tom Trahan <tom@shippable.com>
2017-01-12 15:46:19 -0800

    export variables via bash_profile for use in new parent shells

--- pipeline/install/installGlobal.sh
+++ pipeline/install/installGlobal.sh
@@ -7,12 +7,15 @@ echo "installing Linux tools..."
 DISTRO=alpine
 # DISTRO=ubuntu
 
+# adjust for distro and export variables via .bash_profile for new shells
 if [ $DISTRO == ubuntu ]; then
   export TOOL="sudo apt-get"
   export INSTALL_CMD="apt-get install"
+  echo -e "export TOOL=$TOOL\nexport INSTALL_CMD=$INSTALL_CMD" >> ~/.bash_profile
 elif [ $DISTRO == alpine ]; then
   export TOOL="apk"
   export INSTALL_CMD="apk add"
+  echo -e "export TOOL='$TOOL'\nexport INSTALL_CMD='$INSTALL_CMD'" >> ~/.bash_profile
 else
   echo "Linux distro not supported"
   # exit
