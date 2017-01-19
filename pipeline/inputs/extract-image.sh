7f76bc81086784ac70b617dcaa35895b7dc87a22
Tom Trahan <tom@shippable.com>
2017-01-19 10:48:39 -0800

    extract image information from input [ci skip]

---
+++ pipeline/inputs/extract-image.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Extract params data
+echo -e "\n*** extracting image information ***"
+get_image() {
+  local IMAGE_FILE="./IN/$1/version.json"
+  if [ -f "$IMAGE_FILE" ]; then
+    export DOCKER_REPOSITORY=$(jq -r '.version.propertyBag.yml.pointer.sourceName' $IMAGE_FILE)
+    export TAG=$(jq -r '.version.version.versionName' $IMAGE_FILE)
+    echo "loaded image information"
+  else
+    echo "no params file exists"
+  fi
+}
