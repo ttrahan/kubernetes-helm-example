f4273fc7affe100c768422cac68c4becb2327d90
Tom Trahan <tom@shippable.com>
2017-01-19 08:59:48 -0800

    troubleshoot aws keys in kubecli script [ci skip]

--- pipeline/inputs/extract-integration.sh
+++ pipeline/inputs/extract-integration.sh
@@ -5,6 +5,7 @@
 echo -e "\n*** extracting integration information ***"
 get_integration() {
   local INTEGRATION_FILE="./IN/$1/integration.env"
+  echo "pulling integration file from "$INTEGRATION_FILE
   if [ -f "$INTEGRATION_FILE" ]; then
     . $INTEGRATION_FILE
     echo "loaded integration file"
