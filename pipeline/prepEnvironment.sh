0bf151d71a8d0dad1527f5052c5f050f2c158f0a
Tom Trahan <tom@shippable.com>
2017-01-19 16:21:16 -0800

    make all scripts executable; add load docker options [ci skip]

--- pipeline/prepEnvironment.sh
+++ pipeline/prepEnvironment.sh
@@ -9,6 +9,7 @@ SCRIPT_REPO=$2
 PARAMS_RESOURCE=$3
 INTEGRATION=$4
 IMAGE=$5
+OPTIONS=$6
 
 # source functions used in this script
 for f in $GIT_REPO/pipeline/scriptFunctions/* ; do
@@ -23,6 +24,7 @@ get_previous_statefile $JOB
 get_params $PARAMS_RESOURCE
 get_integration $INTEGRATION
 get_image $IMAGE
+get_docker_options $OPTIONS
 
 # install required CLIs
 install_AwsCli
