80f3bce88aae7095601c67c8ab3f4a35b11836a5
Tom Trahan <tom@shippable.com>
2017-01-19 16:34:02 -0800

    fixes previous state file load [ci skip]

--- pipeline/scriptFunctions/loadPreviousState.sh
+++ pipeline/scriptFunctions/loadPreviousState.sh
@@ -7,7 +7,10 @@ get_previous_statefile() {
   local previous_statefile_location="/build/previousState"
   if [ -f "$previous_statefile_location/*" ]; then
     cp $previous_statefile_location /build/IN/$1/gitRepo
-    echo 'restored previous statefile'
+    echo 'restored previous state'
+    if [ -f "$previous_statefile_location/variable_state.env" ]; then
+      source $previous_statefile_location/variable_state.env
+    fi
   else
     echo "no previous statefile exists"
   fi
