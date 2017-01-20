a1b46a87592939a2cd04e67bed36590ce1149796
Tom Trahan <tom@shippable.com>
2017-01-19 17:21:26 -0800

    remove debug code [ci skip]

--- pipeline/scriptFunctions/loadDockerOptions.sh
+++ pipeline/scriptFunctions/loadDockerOptions.sh
@@ -4,7 +4,6 @@
 get_docker_options() {
   echo -e "\n*** extracting image information ***"
   local OPTIONS_FILE="./IN/$1/version.json"
-  cat $OPTIONS_FILE
   if [ -f "$OPTIONS_FILE" ]; then
     export MEMORY=$(jq -r '.version.propertyBag.memory' $OPTIONS_FILE)
     export CPU=$(jq -r '.version.propertyBag.cpuShares' $OPTIONS_FILE)
