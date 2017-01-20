8d8cca4b27f8880d486d76142ade564bf23449bf
Tom Trahan <tom@shippable.com>
2017-01-19 16:05:06 -0800

    cleans up save state scripts [ci skip]

--- pipeline/scriptFunctions/saveState.sh
+++ pipeline/scriptFunctions/saveState.sh
@@ -4,21 +4,16 @@
 echo -e "\n*** saving state ***"
 save_state_variables() {
   STATE_SAVE_LOCATION=/build/state/
-  NUM_PARAMS=$#
   for var in "$@"; do
     echo -e "export "$var"="${!var} >> $STATE_SAVE_LOCATION/variable_state.env
   done
+  echo "variables saved to state successfully"
 }
 
 save_state_files() {
-  STATE_SAVE_LOCATION=.
-  NUM_PARAMS=$#
+  STATE_SAVE_LOCATION=/build/state/
   for file in "$@"; do
     cp $file $STATE_SAVE_LOCATION
   done
+  echo "files saved to state successfully"
 }
-
-# ENV_VAR_TO_SAVE=$i
-# for var in "${ENV_VAR_TO_SAVE[@]}"; do
-#   echo -e "export "$var"="${!var} >> $STATE_SAVE_LOCATION/variable_state.env
-# done
