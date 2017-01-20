9297684c4431dc82b5c321d03019f4ca80185627
Tom Trahan <tom@shippable.com>
2017-01-19 16:59:02 -0800

    troubleshoot jq errors [ci skip]

--- pipeline/scriptFunctions/loadDockerOptions.sh
+++ pipeline/scriptFunctions/loadDockerOptions.sh
@@ -4,6 +4,7 @@
 get_docker_options() {
   echo -e "\n*** extracting image information ***"
   local OPTIONS_FILE="./IN/$1/version.json"
+  cat $OPTIONS_FILE
   if [ -f "$OPTIONS_FILE" ]; then
     export MEMORY=$(jq -r .[]'.images[].dockerOptions.memory' $OPTIONS_FILE)
     export CPU=$(jq -r .[]'.images[].dockerOptions.cpuShares' $OPTIONS_FILE)
