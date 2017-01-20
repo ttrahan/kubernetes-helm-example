988d215f78a129925509dfaf3d5572da4177f35a
Tom Trahan <tom@shippable.com>
2017-01-19 16:40:41 -0800

    fixes parameter used in docker options script [ci skip]

--- pipeline/scriptFunctions/loadDockerOptions.sh
+++ pipeline/scriptFunctions/loadDockerOptions.sh
@@ -3,7 +3,7 @@
 # Extract params data
 get_docker_options() {
   echo -e "\n*** extracting image information ***"
-  local OPTIONS_FILE="./IN/$6/version.json"
+  local OPTIONS_FILE="./IN/$1/version.json"
   if [ -f "$OPTIONS_FILE" ]; then
     export MEMORY=$(jq -r .[]'.images[].dockerOptions.memory' $OPTIONS_FILE)
     export CPU=$(jq -r .[]'.images[].dockerOptions.cpuShares' $OPTIONS_FILE)
