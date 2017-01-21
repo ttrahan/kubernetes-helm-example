9b151a0943b39207967a3d1ccdcf7883c85cfd56
Tom Trahan <tom@shippable.com>
2017-01-20 17:00:23 -0800

    convert runSh scripts to use Shippable injected environment variables

--- pipeline/prepEnvironment.sh
+++ pipeline/prepEnvironment.sh
@@ -1,30 +1,39 @@
 #! /bin/sh -e
 
-# location of source files referenced in this script
-GIT_REPO="/build/IN/repo-sample-kube/gitRepo/"
+# assign Shippable variables for use in scripts
+export ENVIRONMENT=$PARAMSSAMPLETESTKUBE_PARAMS_ENVIRONMENT
+export PORT=$PARAMSSAMPLETESTKUBE_PARAMS_PORT
+export GIT_REPO=$REPOSAMPLEKUBE_PATH
+export DOCKER_REPOSITORY_SAMPLE=$IMGSAMPLEKUBE_POINTER_SOURCENAME
+export TAG_SAMPLE=$IMGSAMPLEKUBE_VERSION_VERSIONNAME
+export MEMORY=$IMGOPTSSAMPLEKUBETEST_VERSION_MEMORY
+export CPU=$IMGOPTSSAMPLEKUBETEST_VERSION_CPUSHARES
 
-# assign parameters passed into this script
-JOB=$1
-SCRIPT_REPO=$2
-PARAMS_RESOURCE=$3
-INTEGRATION=$4
-IMAGE=$5 # will come from either image resource or predecessor deploy job
-OPTIONS=$6
+# # location of source files referenced in this script
+# GIT_REPO="/build/IN/repo-sample-kube/gitRepo/"
+
+# # assign parameters passed into this script
+# JOB=$1
+# SCRIPT_REPO=$2
+# PARAMS_RESOURCE=$3
+# INTEGRATION=$4
+# IMAGE=$5 # will come from either image resource or predecessor deploy job
+# OPTIONS=$6
 
 # source functions used in this script
-for f in $GIT_REPO/pipeline/scriptFunctions/* ; do
+for f in $GIT_REPO/pipeline/scriptFunctions/*.* ; do
   source $f ;
 done
 
 # install shell tools required
 install_ShellTools
 
-# process inputs into environment
-get_previous_statefile $JOB
-get_params $PARAMS_RESOURCE
-get_integration $INTEGRATION
-get_image $IMAGE
-get_docker_options $OPTIONS
+# # process inputs into environment
+# get_previous_statefile $JOB
+# get_params $PARAMS_RESOURCE
+# get_integration $INTEGRATION
+# get_image $IMAGE
+# get_docker_options $OPTIONS
 
 # install required CLIs
 install_AwsCli
