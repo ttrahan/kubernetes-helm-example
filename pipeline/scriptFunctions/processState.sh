476c2fe5cb059a6e39cea6c1e80d1033680343be
Tom Trahan <tom@shippable.com>
2017-01-23 16:41:18 -0800

    add loadState functions [ci skip]

--- pipeline/scriptFunctions/processState.sh
+++ pipeline/scriptFunctions/processState.sh
@@ -4,6 +4,7 @@
 echo -e "\n*** saving state ***"
 save_state_variables() {
   STATE_SAVE_LOCATION=/build/state/
+  # process each parameter passed in
   for var in "$@"; do
     echo -e "export "$var"="${!var} >> $STATE_SAVE_LOCATION/variable_state.env
   done
@@ -12,23 +13,19 @@ save_state_variables() {
 
 save_state_files() {
   STATE_SAVE_LOCATION=/build/state/
+  # process each parameter passed in
   for file in "$@"; do
     cp $file $STATE_SAVE_LOCATION
   done
   echo "files saved to state successfully"
 }
 
-# NEED TO UPDATE THESE TWO SCRIPTS!!!
 # Load state
 echo -e "\n*** loading state ***"
 load_state_variables() {
   STATE_LOAD_LOCATION=/build/previousState
-  source $STATE_LOAD_LOCATION/variable_state.env
-  echo "variables loaded to state successfully"
-}
-
-load_state_files() {
-  STATE_SAVE_LOCATION=/build/previousState
-  
-  echo "files loaded to state successfully"
+  if [[ -f $STATE_LOAD_LOCATION/variable_state.env ]]; then
+    source $STATE_LOAD_LOCATION/variable_state.env
+    echo "variables loaded to state successfully"
+  fi
 }
