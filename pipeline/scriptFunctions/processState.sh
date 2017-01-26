5cebffb7594b5e4960b2a1693e98d29ecda4e224
Tom Trahan <tom@shippable.com>
2017-01-26 15:20:47 -0800

    update saveState scripts [ci skip]

--- pipeline/scriptFunctions/processState.sh
+++ pipeline/scriptFunctions/processState.sh
@@ -1,38 +1,38 @@
 #! /bin/bash
 
 # Save state
-echo -e "\n*** saving state ***"
 save_state_variables() {
   STATE_SAVE_LOCATION=/build/state/
+  echo -e "\n*** saving state - variables ***"
   # process each parameter passed in
   for var in "$@"; do
     if [[ ! -z $var ]]; then
-      echo -e "export "$var_PREVIOUS"="${!var} >> $STATE_SAVE_LOCATION/variable_state.env
+      echo "export "$var"_PREVIOUS="${!var} >> $STATE_SAVE_LOCATION/variable_state.env
     else
       echo "variable not found or is empty...state not saved"
     fi
   done
-  echo "variables saved to state successfully"
   cat $STATE_SAVE_LOCATION/variable_state.env
 }
 
 save_state_files() {
   STATE_SAVE_LOCATION=/build/state/
+  echo -e "\n*** saving state - files ***"
   # process each parameter passed in
   for file in "$@"; do
     if [[ -f $file ]]; then
       cp $file $STATE_SAVE_LOCATION
+      echo "files saved to state successfully"
     else
       echo "file requested is not found in location specified...state not saved"
     fi
   done
-  echo "files saved to state successfully"
 }
 
 # Load state
-echo -e "\n*** loading state ***"
 load_state_variables() {
   STATE_LOAD_LOCATION=/build/previousState
+  echo -e "\n*** loading state - variables ***"
   if [[ -f $STATE_LOAD_LOCATION/variable_state.env ]]; then
     cat $STATE_LOAD_LOCATION/variable_state.env
     source $STATE_LOAD_LOCATION/variable_state.env
