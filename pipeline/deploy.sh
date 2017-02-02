262510ee5ce16e72aae0a4fec9fa9899a32c723c
Tom Trahan <tom@shippable.com>
2017-02-02 10:28:20 -0800

    change helm command to upgrade if already exists [ci skip]

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -12,7 +12,7 @@ envsubst < ./chartSample/values-template.yaml > ./chartSample/values.yaml
 
 # deploy to Kubernetes cluster via Helm
 echo "deploying via Helm"
-helm install ./chartSample
+helm upgrade --install ./chartSample
 
 # # save State to be used in subsequent jobs or next time this job runs. Pass variable names or files (with path) as parameters
 save_state_variables SAMPLE_IMAGE_URL SAMPLE_IMAGE_TAG NGINX_IMAGE_URL NGINX_IMAGE_TAG
